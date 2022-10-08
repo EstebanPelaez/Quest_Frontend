32e7c759747d5c1efad21d48d6c6165da626163b
EstebanPelaez <87784235+EstebanPelaez@users.noreply.github.com>
2022-10-08 12:36:52 -0500

    Results
    Se crea la pantalla de los resultados de los usuarios al terminar el test y se revisan las preguntas correctas y las incorrectas con su retroalimentación.

--- FECuestionarios/src/app/Components/results/results.component.spec.ts
+++ FECuestionarios/src/app/Components/results/results.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultsComponent } from './results.component';
 
+
 describe('ResultsComponent', () => {
   let component: ResultsComponent;
   let fixture: ComponentFixture<ResultsComponent>;
